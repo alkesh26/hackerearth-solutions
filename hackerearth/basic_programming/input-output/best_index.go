c2ee797742f7a7df581407c2e3f6262446a71488
alkesh26 <alkesh26@gmail.com>
2019-05-30 15:28:11 +0530

    Update best_index.go

--- hackerearth/basic_programming/input-output/best_index.go
+++ hackerearth/basic_programming/input-output/best_index.go
@@ -1,3 +1,4 @@
+// Best Index Hackerearth 
 // Problem link:
 // https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/best-index-1-45a2f8ff/
 
